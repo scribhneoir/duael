b8dee2251682e0801b1155525ff2160e7bc59a55
Josiah McCracken <josiah.m@boostpoint.com>
2022-09-08 14:56:55 -0400

    update icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.duael.app',
   appName: 'DUæL',
   webDir: 'dist',
-  bundledWebRuntime: false,
-  //TODO: remove in production. Used for live reload in a dev env
-  server: {
-    url: 'http://127.0.0.1:5173/'
-  }
+  bundledWebRuntime: false
 };
 
 export default config;
